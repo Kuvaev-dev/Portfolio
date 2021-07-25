0f558e620c10eb8e595977a5db9cb3f6339e35a2
Никита Куваев <likeaboss13371@gmail.com>
2021-07-25 21:35:15 +0300

    Category Filter Add

---
+++ js/dist/app.dev.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// создаём фнкцию для взаимодействия со средой
+$(function () {
+  var filter = $("[data-filter]"); // сохраняем элемент сайта в переменную
+
+  filter.on("click", function (event) {
+    // подвязываем событие клика
+    event.preventDefault(); // убираем стандартное поведение элемента    
+
+    var cat = $(this).data('filter'); // помещаем категорию в переменную
+
+    if (cat == 'all') {
+      // если выбрана категория "all"
+      $("[data-cat]").removeClass('hide'); // открываем все элементы
+    } else {
+      // если выбрана другая категория
+      $("[data-cat]").each(function () {
+        // делаем аналог foreach 
+        var workCat = $(this).data('cat'); // проходим по всем элементам cat
+
+        if (workCat != cat) {
+          // если элемент не совпадает с категорией
+          $(this).addClass('hide'); // обращаемся к классу hide и скрываем элементы других категорий
+        } else {
+          $(this).removeClass('hide'); // в противном случае открываем их
+        }
+      });
+    }
+  });
+});
